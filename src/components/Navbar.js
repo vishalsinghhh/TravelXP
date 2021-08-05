174ab93c731911a1e602508f490b3d004cafc226
vishalsinghhh <vishalsingh0710vs@gmail.com>
2021-08-05 23:21:05 +0530

    New Changes

---
+++ src/components/Navbar.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled, {css} from 'styled-components/macro';
+import { Link } from 'react-router-dom';
+import { menuData } from '../data/MenuData';
+
+const Nav = styled.nav`
+    height: 60px;
+    display: flex;
+    justify-content: space-between;
+    padding: 1rem 2rem;
+    z-index: 100;
+    position: fixed;
+    width: 100%;
+    background: #000;
+`;
+
+const NavLink = css`
+    color: white;
+    display: flex;
+    align-items: center;
+    padding: 0 1rem;
+    height: 100%;
+    cursor: pointer;
+    text-decoration: none;
+`
+
+const Logo = styled(Link)`
+    ${NavLink}
+    color: #fff;
+    font-style: italic;
+`
+const MenuBars = styled.i``
+const NavMenu = styled.div`
+    display: flex;
+    align-items: center;
+`
+const NavMenuLinks = styled(Link)`
+    ${NavLink}
+`;
+
+const Navbar = () => {
+    return (
+        <Nav>
+            <Logo to="/">TravelXP</Logo>
+            <MenuBars />
+            <NavMenu>
+                {menuData.map((item, index)=>(
+                    <NavMenuLinks to={item.link} key={index}>
+                        {item.title}
+                    </NavMenuLinks>
+                ))}
+            </NavMenu>
+        </Nav>
+    )
+}
+
+export default Navbar
